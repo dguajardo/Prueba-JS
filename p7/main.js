e8d23abedb5ee096375f661c5a05b95074f89dfc
Diego Guajardo <diego.guajardo@hotmail.cl>
2016-02-26 22:01:23 -0300

    p7

--- p7/main.js
+++ p7/main.js
@@ -1,6 +1,15 @@
-function Phone(number) {
- this.dinero = dinero
+function Phone(brand, color, number) {
+this.brand = brand;
+this.modelo = color;
+this.año = color;
 
- this.ingresar = function (cantidad) {
- this.dinero = this.dinero + cantidad
- }
+	this.dial = function (call) {
+	this.call += this.call 
+	}
+
+	var phones= [];
+	this.brands = function(phones){
+		for (var i =0; i <= phones.lenght; i++	) {
+		console.log(phones[i]);
+		}
+	}	
